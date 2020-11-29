5eba1b13680fae0d57300eb39b1f42e9f5723040
Al Plunge <al.plunge@gmail.com>
2020-11-29 21:18:17 +0200

    optimised code

--- src/main/java/banking/ap/App.java
+++ src/main/java/banking/ap/App.java
@@ -9,7 +9,6 @@ import java.io.IOException;
 public class App {
     public static void main(String[] args) throws IOException {
         final BankStatementAnalyzer bankStatementAnalyzer = new BankStatementAnalyzer();
-        final BankTransactionFilter bankTransactionFilter = new BankTransactionIsInAprilAndExpensive();
         final BankStatementParser bankStatementParser = new BankStatementCSVParser();
         final Exporter exporter = new JSONExporter();
         bankStatementAnalyzer.analyze(args[0], bankStatementParser, exporter);
